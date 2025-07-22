e3366830ab7dd3090bcc8f85490d9254d23ef334
Sergio Canales <sergio.canales.e@gmail.com>
2025-07-22 08:36:12 -0400

    Configure OIDC to use ID token claims

--- quarkus-app/src/main/java/com/scanales/eventflow/private_/ProfileResource.java
+++ quarkus-app/src/main/java/com/scanales/eventflow/private_/ProfileResource.java
@@ -5,8 +5,6 @@ import io.quarkus.qute.TemplateInstance;
 import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
 import org.jboss.logging.Logger;
-import org.eclipse.microprofile.jwt.JsonWebToken;
-import jakarta.enterprise.inject.Instance;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -31,50 +29,18 @@ public class ProfileResource {
     @Inject
     SecurityIdentity identity;
 
-    @Inject
-    Instance<JsonWebToken> jwtInstance;
-
-
     @GET
     @Authenticated
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance profile() {
         identity.getAttributes().forEach((k, v) -> LOG.infov("{0} = {1}", k, v));
+        String name = identity.getAttribute("name");
+        String givenName = identity.getAttribute("given_name");
+        String familyName = identity.getAttribute("family_name");
+        String email = identity.getAttribute("email");
 
-        JsonWebToken jwt = null;
-        try {
-            jwt = jwtInstance.get();
-        } catch (Exception ex) {
-            LOG.debugf("JWT unavailable: %s", ex.getMessage());
-        }
-
-        String name = null;
-        String givenName = null;
-        String familyName = null;
-        String email = null;
-
-        if (jwt != null) {
-            name = jwt.getClaim("name");
-            givenName = jwt.getClaim("given_name");
-            familyName = jwt.getClaim("family_name");
-            email = jwt.getClaim("email");
-        }
-
-        if (name == null) {
-            name = identity.getAttribute("name");
-        }
         if (name == null) {
             name = identity.getPrincipal().getName();
-        }
-
-        if (givenName == null) {
-            givenName = identity.getAttribute("given_name");
-        }
-        if (familyName == null) {
-            familyName = identity.getAttribute("family_name");
-        }
-        if (email == null) {
-            email = identity.getAttribute("email");
         }
 
         String sub = identity.getPrincipal().getName();
