b4016c739f7cb737c8bae28b2364065814ce6b59
Sergio Canales <sergio.canales.e@gmail.com>
2025-08-06 20:43:53 -0400

    Unify event agenda into event detail

--- quarkus-app/src/main/java/com/scanales/eventflow/public_/EventResource.java
+++ quarkus-app/src/main/java/com/scanales/eventflow/public_/EventResource.java
@@ -12,8 +12,6 @@ import jakarta.ws.rs.core.MediaType;
 import com.scanales.eventflow.service.EventService;
 import com.scanales.eventflow.model.Event;
 import jakarta.inject.Inject;
-import io.quarkus.security.identity.SecurityIdentity;
-import com.scanales.eventflow.service.UserScheduleService;
 
 @Path("/event")
 public class EventResource {
@@ -21,17 +19,11 @@ public class EventResource {
     @CheckedTemplate
     static class Templates {
         static native TemplateInstance detail(Event event);
-        static native TemplateInstance agenda(Event event, java.util.Set<String> userTalks);
     }
 
     @Inject
     EventService eventService;
 
-    @Inject
-    SecurityIdentity identity;
-
-    @Inject
-    UserScheduleService userSchedule;
 
     @GET
     @Path("{id}")
@@ -43,22 +35,5 @@ public class EventResource {
             return Templates.detail(null);
         }
         return Templates.detail(event);
-    }
-
-    @GET
-    @Path("{id}/agenda")
-    @PermitAll
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance agenda(@PathParam("id") String id) {
-        Event event = eventService.getEvent(id);
-        java.util.Set<String> talks = java.util.Set.of();
-        if (identity != null && !identity.isAnonymous()) {
-            String email = identity.getAttribute("email");
-            if (email == null) {
-                email = identity.getPrincipal().getName();
-            }
-            talks = userSchedule.getTalksForUser(email);
-        }
-        return Templates.agenda(event, talks);
     }
 }
