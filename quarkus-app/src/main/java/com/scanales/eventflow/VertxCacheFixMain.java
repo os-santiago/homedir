9277eb1477ee9e6c1cc7d5dcb27c4f40ef5f9ceb
Sergio Canales <sergio.canales.e@gmail.com>
2025-08-18 12:20:46 -0400

    Add custom main to disable Vert.x file caching

---
+++ quarkus-app/src/main/java/com/scanales/eventflow/VertxCacheFixMain.java
@@ -0,0 +1,17 @@
+package com.scanales.eventflow;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+/**
+ * Custom entry point that disables Vert.x file caching before the application starts.
+ */
+@QuarkusMain
+public class VertxCacheFixMain {
+
+    public static void main(String... args) {
+        // Disable Vert.x file caching to avoid permission warnings on some filesystems
+        System.setProperty("vertx.disableFileCaching", "true");
+        Quarkus.run(args);
+    }
+}
