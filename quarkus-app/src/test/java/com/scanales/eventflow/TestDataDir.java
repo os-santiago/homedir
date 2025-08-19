95125c508ed2dbd0dfaecad6d52e400ba95ae0ae
Sergio Canales <sergio.canales.e@gmail.com>
2025-08-19 09:23:56 -0400

    Use temp data dir for tests

---
+++ quarkus-app/src/test/java/com/scanales/eventflow/TestDataDir.java
@@ -0,0 +1,27 @@
+package com.scanales.eventflow;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class TestDataDir implements QuarkusTestResourceLifecycleManager {
+    private Path tempDir;
+
+    @Override
+    public Map<String, String> start() {
+        try {
+            tempDir = Files.createTempDirectory("eventflow-test");
+            String path = tempDir.toString();
+            System.setProperty("eventflow.data.dir", path);
+            return Map.of("eventflow.data.dir", path);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        // no cleanup required
+    }
+}
