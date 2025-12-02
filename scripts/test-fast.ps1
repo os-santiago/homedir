975945e388c270d901dee07b93b071c8ce76cff4
Sergio Canales <sergio.canales.e@gmail.com>
2025-12-01 23:24:13 -0300

    revision local, push para codex, revision en cloud

--- scripts/test-fast.ps1
+++ scripts/test-fast.ps1
@@ -4,6 +4,6 @@ $ErrorActionPreference = 'Stop'
 Push-Location (Split-Path $MyInvocation.MyCommand.Path -Parent) | Out-Null
 Set-Location ..
 
-mvn -f quarkus-app/pom.xml -T 1C -Dquarkus.devservices.enabled=false -DskipITs=true test
+mvn -f quarkus-app/pom.xml -T 1C "-Dquarkus.devservices.enabled=false" "-DskipITs=true" test
 
 Pop-Location | Out-Null
