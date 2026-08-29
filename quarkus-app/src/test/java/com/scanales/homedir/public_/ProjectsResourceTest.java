04622ebfb4ba1f1c79dec35ef7316640b23e7c00
VERCTORG99 <dh8939693@gmail.com>
2026-08-28 21:12:06 -0400

    test: assert meta description tag in proyectos render
    Strengthens proyectosMetaDescriptionIsLocalizedToDefaultLocale to verify the rendered <meta name="description"> tag (EN active; ES still disabled per #1267).
    
    Signed-off-by: VERCTORG99 <dh8939693@gmail.com>

--- quarkus-app/src/test/java/com/scanales/homedir/public_/ProjectsResourceTest.java
+++ quarkus-app/src/test/java/com/scanales/homedir/public_/ProjectsResourceTest.java
@@ -46,7 +46,7 @@ public class ProjectsResourceTest {
         .statusCode(200)
         .body(
             containsString(
-                "Explore open-source projects and community initiatives from OpenSource Santiago."));
+                "<meta name=\"description\" content=\"Explore open-source projects and community initiatives from OpenSource Santiago.\""));
   }
 
   @Test
